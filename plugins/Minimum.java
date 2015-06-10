67f25dc087e5dd9845305b5e52826a1590132673
Akki5 <deadlyaakarsh200@gmail.com>
2015-06-11 00:00:22 +0530

    Updated version

---
+++ plugins/Minimum.java
@@ -0,0 +1,29 @@
+
+/**
+ * This plugin returns maximum of the data list.
+ */
+ 
+import java.util.Arrays;
+public class Minimum implements StatisticalAlgo {
+
+	int A[];
+	
+	public String getPluginName() {
+		return "Minimum";
+	}
+
+	public void setParameter (int param[]) {
+		A = param;
+	}
+
+	public double getResult() {
+		Arrays.sort(A);
+		return A[0];
+	}
+
+	/* yes, ths operation can fail, but we are going to ignore this here
+	public boolean hasError() {
+		return false;
+	}*/
+}
+
