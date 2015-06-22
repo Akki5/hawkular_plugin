bb170544069ae5e4f487610567aacd9a99de74d6
Aakarsh Agarwal <deadlyaakarsh200@gmail.com>
2015-06-22 20:18:47 +0530

    Updated version

--- src/test/java/org/hawkular/PluginDemoTest.java
+++ src/test/java/org/hawkular/PluginDemoTest.java
@@ -11,13 +11,11 @@ import static org.junit.Assert.*;
 public class PluginDemoTest {
 
 	@Test
-	public void checkResult() throws Exception
+	public void checkAllPlugins() throws Exception
 	{
-		Scanner in = new Scanner(System.in);
 		
-		//initializing a static array for test purposes
+		//initializing an array for test purposes
 		int A[] = new int[]{3,5,9,2,4,7};
-		int choice=0;
 		double result[] = new double[]{5.0,9.0,2.0,4.5,2.6076809620810595};
 		
 		PluginDemo demo = new PluginDemo(args);
@@ -26,13 +24,9 @@ public class PluginDemoTest {
 		demo.getPlugins();
 		
 		// take input from user and use the concerned plugin
-		for(;;)
+		for(i=0;i<demo.no_of_plugins();i++)
 		{
-			System.out.println("Enter the choice number corresponding to the required mathematical operation.\n'1' for Average\n'2' for Maximum\n'3' for Minimum\n'4' for Mode\n'5' for Standard Deviation\n'0' to quit");
-			choice = in.nextInt();
-			if(choice==0)
-				break;
-			assertEquals(result[choice-1],demo.runPlugins(choice-1,A));
+			assertEquals(result[i],demo.runPlugins(i,A));
 		}
 	}
 
