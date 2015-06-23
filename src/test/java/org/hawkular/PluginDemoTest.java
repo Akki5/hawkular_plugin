6dec39cc8944abdcf914dcfa1bc54432d10787d4
Aakarsh Agarwal <deadlyaakarsh200@gmail.com>
2015-06-23 13:16:27 +0530

    Update PluginDemoTest.java

--- src/test/java/org/hawkular/PluginDemoTest.java
+++ src/test/java/org/hawkular/PluginDemoTest.java
@@ -10,15 +10,16 @@ import static org.junit.Assert.*;
 
 public class PluginDemoTest {
 
+	//initializing an array for test purposes
+	int A[] = new int[]{3,5,9,2,4,7};
+	// storing pre-defined results
+	double result[] = new double[]{5.0,9.0,2.0,4.5,2.6076809620810595};
+	
+	PluginDemo demo = new PluginDemo(args);
+
 	@Test
 	public void checkAllPlugins() throws Exception
 	{
-		
-		//initializing an array for test purposes
-		int A[] = new int[]{3,5,9,2,4,7};
-		double result[] = new double[]{5.0,9.0,2.0,4.5,2.6076809620810595};
-		
-		PluginDemo demo = new PluginDemo(args);
 		
 		// load all classes in plugin directory via classloader class
 		demo.getPlugins();
@@ -29,6 +30,106 @@ public class PluginDemoTest {
 			assertEquals(result[i],demo.runPlugins(i,A));
 		}
 	}
-
+	
+	@Test
+	public void checkPluginAverage() throws Exception
+	{
+		
+		// load all classes in plugin directory via classloader class
+		demo.getPlugins();
+		
+		String pluginName = "Average";
+		
+		// take input from user and use the concerned plugin
+		for(i=0;i<demo.no_of_plugins();i++)
+		{
+			if((demo.plugins(i).getPluginName()).equals(pluginName))
+			{
+				assertEquals(result[i],demo.runPlugins(i,A));
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void checkPluginMaximum() throws Exception
+	{
+		
+		// load all classes in plugin directory via classloader class
+		demo.getPlugins();
+		
+		String pluginName = "Maximum";
+		
+		// take input from user and use the concerned plugin
+		for(i=0;i<demo.no_of_plugins();i++)
+		{
+			if((demo.plugins(i).getPluginName()).equals(pluginName))
+			{
+				assertEquals(result[i],demo.runPlugins(i,A));
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void checkPluginMinimum() throws Exception
+	{
+		
+		// load all classes in plugin directory via classloader class
+		demo.getPlugins();
+		
+		String pluginName = "Minimum";
+		
+		// take input from user and use the concerned plugin
+		for(i=0;i<demo.no_of_plugins();i++)
+		{
+			if((demo.plugins(i).getPluginName()).equals(pluginName))
+			{
+				assertEquals(result[i],demo.runPlugins(i,A));
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void checkPluginMode() throws Exception
+	{
+		
+		// load all classes in plugin directory via classloader class
+		demo.getPlugins();
+		
+		String pluginName = "Mode";
+		
+		// take input from user and use the concerned plugin
+		for(i=0;i<demo.no_of_plugins();i++)
+		{
+			if((demo.plugins(i).getPluginName()).equals(pluginName))
+			{
+				assertEquals(result[i],demo.runPlugins(i,A));
+				break;
+			}
+		}
+	}
+	
+	@Test
+	public void checkPluginStdDev() throws Exception
+	{
+		
+		// load all classes in plugin directory via classloader class
+		demo.getPlugins();
+		
+		String pluginName = "Standard Deviation";
+		
+		// take input from user and use the concerned plugin
+		for(i=0;i<demo.no_of_plugins();i++)
+		{
+			if((demo.plugins(i).getPluginName()).equals(pluginName))
+			{
+				assertEquals(result[i],demo.runPlugins(i,A));
+				break;
+			}
+		}
+	}
+	
 }
 
