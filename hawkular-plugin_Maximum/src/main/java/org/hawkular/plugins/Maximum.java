281d1cf45a91e64f4b1f14af90eb0e3e1b3efb50
Aakarsh Agarwal <deadlyaakarsh200@gmail.com>
2015-07-10 17:24:46 +0530

    Update Maximum.java

--- hawkular-plugin_Maximum/src/main/java/org/hawkular/plugins/Maximum.java
+++ hawkular-plugin_Maximum/src/main/java/org/hawkular/plugins/Maximum.java
@@ -7,19 +7,37 @@ import java.util.Arrays;
 import org.hawkular.*;
 public class Maximum implements StatisticalAlgo {
 
-	int A[];
+	List elements;
+	int window_size;
+	
+	Maximum(int window_size)
+	{
+		this.window_size = window_size;
+		elements = new ArrayList();
+	}
 	
 	public String getPluginName() {
 		return "Maximum";
 	}
 
-	public void setParameter (int param[]) {
-		A = param;
+	public void setPoints (List values) {
+		elements = values;
+	}
+	
+	public void pushPoint (int value) {
+		if(elements.size()<window_size)
+			elements.add(value);
+		else
+		{
+			for(int i=0;i<window_size;i++)
+				elements[i] = elements[i+1];
+			elements[window_size-1] = value;
+		}
 	}
 
 	public double getResult() {
-		Arrays.sort(A);
-		return A[A.length-1];
+		Arrays.sort(elements);
+		return elements[elements.length-1];
 	}
 
 	/* yes, ths operation can fail, but we are going to ignore this here
