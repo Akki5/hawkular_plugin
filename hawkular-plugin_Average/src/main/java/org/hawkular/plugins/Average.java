be5ebdd359a5ba80eddf23ee2ca2e438d44175a6
Aakarsh Agarwal <deadlyaakarsh200@gmail.com>
2015-07-10 17:16:23 +0530

    Update Average.java

--- hawkular-plugin_Average/src/main/java/org/hawkular/plugins/Average.java
+++ hawkular-plugin_Average/src/main/java/org/hawkular/plugins/Average.java
@@ -8,21 +8,43 @@ import java.util.Arrays;
 import org.hawkular.*;
 public class Average implements StatisticalAlgo {
 
-	int A[];
+	List elements;
+	int window_size;
 	
 	public String getPluginName() {
 		return "Average";
 	}
 
-	public void setParameter (int param[]) {
-		A = param;
+	Average(int window_size)
+	{
+		this.window_size = window_size;
+		elements = new ArrayList();
+	}
+	
+	public String getPluginName() {
+		return "Average";
+	}
+
+	public void setPoints (List values) {
+		elements = values;
+	}
+	
+	public void pushPoint (int value) {
+		if(elements.size()<window_size)
+			elements.add(value);
+		else
+		{
+			for(int i=0;i<window_size;i++)
+				elements[i] = elements[i+1];
+			elements[window_size-1] = value;
+		}
 	}
 
 	public double getResult() {
-		int size = A.length,sum=0;
+		int size = elements.length,sum=0;
 		double avg=0.0;
 		for(int i=0;i<size;i++)
-			sum+=A[i];
+			sum+=elements[i];
 		avg = sum/(size*1.0);
 		return avg;
 	}
@@ -32,4 +54,3 @@ public class Average implements StatisticalAlgo {
 		return false;
 	}
 }
-
