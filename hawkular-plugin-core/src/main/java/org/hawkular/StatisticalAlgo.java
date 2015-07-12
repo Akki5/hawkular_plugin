3c87915353e8b7d2d84068005a22dacca60077c3
Aakarsh Agarwal <deadlyaakarsh200@gmail.com>
2015-07-12 23:12:14 +0530

    Update StatisticalAlgo.java

--- hawkular-plugin-core/src/main/java/org/hawkular/StatisticalAlgo.java
+++ hawkular-plugin-core/src/main/java/org/hawkular/StatisticalAlgo.java
@@ -4,9 +4,10 @@ public interface StatisticalAlgo {
 
 	// return the name of this plugin
 	public String getPluginName();
-		
-	// let the application pass in a parameter
-	public void setParameter (int param[]);
+	
+	public void set_params(int size);
+
+	public void pushPoint (double value);
 
 	// retrieve a result from the plugin
 	public double getResult();
