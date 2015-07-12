5ffd9a5870060bb9ad96e0b17ce1922209f41cfb
Aakarsh Agarwal <deadlyaakarsh200@gmail.com>
2015-07-12 23:04:23 +0530

    Update Maximum.java

--- hawkular-plugin_Maximum/src/main/java/org/hawkular/plugins/Maximum.java
+++ hawkular-plugin_Maximum/src/main/java/org/hawkular/plugins/Maximum.java
@@ -3,41 +3,43 @@
  */
  package org.hawkular.plugins;
  
-import java.util.Arrays;
+import java.util.*;
 import org.hawkular.*;
 public class Maximum implements StatisticalAlgo {
 
-	List elements;
+	List<Double> elements;
 	int window_size;
-	
-	Maximum(int window_size)
-	{
-		this.window_size = window_size;
-		elements = new ArrayList();
-	}
 	
 	public String getPluginName() {
 		return "Maximum";
 	}
 
-	public void setPoints (List values) {
-		elements = values;
-	}
-	
-	public void pushPoint (int value) {
-		if(elements.size()<window_size)
-			elements.add(value);
-		else
-		{
-			for(int i=0;i<window_size;i++)
-				elements[i] = elements[i+1];
-			elements[window_size-1] = value;
-		}
+	public void set_params(int size) {
+		elements = new ArrayList<Double>();
+		window_size = size;
 	}
 
+	public void pushPoint (double value) {
+		if(elements.size()<window_size)
+			elements.add(value*1.0);
+		else
+		{
+			for(int i=0;i<window_size-1;i++)
+			{
+				elements.set(i,elements.get(i+1));
+			}
+			elements.set(window_size-1, value*1.0);
+		}
+	}
+	
 	public double getResult() {
-		Arrays.sort(elements);
-		return elements[elements.length-1];
+	
+		int size = elements.size();
+		if(size<window_size)
+			return -1.0;
+			
+		Collections.sort(elements);
+		return elements.get(window_size-1)*1.0;
 	}
 
 	/* yes, ths operation can fail, but we are going to ignore this here
