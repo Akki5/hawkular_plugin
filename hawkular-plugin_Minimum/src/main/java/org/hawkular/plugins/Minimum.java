8ba78ee8a7b043db174485f3ee042c8ecef81813
Aakarsh Agarwal <deadlyaakarsh200@gmail.com>
2015-07-12 23:06:59 +0530

    Update Minimum.java

--- hawkular-plugin_Minimum/src/main/java/org/hawkular/plugins/Minimum.java
+++ hawkular-plugin_Minimum/src/main/java/org/hawkular/plugins/Minimum.java
@@ -3,23 +3,43 @@
  */
  package org.hawkular.plugins;
  
-import java.util.Arrays;
+import java.util.*;
 import org.hawkular.*;
 public class Minimum implements StatisticalAlgo {
 
-	int A[];
+	List<Double> elements;
+	int window_size;
 	
 	public String getPluginName() {
-		return "Minimum";
+		return "Maximum";
 	}
 
-	public void setParameter (int param[]) {
-		A = param;
+	public void set_params(int size) {
+		elements = new ArrayList<Double>();
+		window_size = size;
+	}
+
+	public void pushPoint (double value) {
+		if(elements.size()<window_size)
+			elements.add(value);
+		else
+		{
+			for(int i=0;i<window_size-1;i++)
+			{
+				elements.set(i,elements.get(i+1));
+			}
+			elements.set(window_size-1, value);
+		}
 	}
 
 	public double getResult() {
-		Arrays.sort(A);
-		return A[0];
+	
+		int size = elements.size();
+		if(size<window_size)
+			return -1.0;
+			
+		Collections.sort(elements);
+		return elements.get(0);
 	}
 
 	/* yes, ths operation can fail, but we are going to ignore this here
