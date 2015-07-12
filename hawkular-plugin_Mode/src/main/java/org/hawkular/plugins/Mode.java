8113cccdf2c818a9f4237c1de7afc9c3c81f0317
Aakarsh Agarwal <deadlyaakarsh200@gmail.com>
2015-07-12 23:07:45 +0530

    Update Mode.java

--- hawkular-plugin_Mode/src/main/java/org/hawkular/plugins/Mode.java
+++ hawkular-plugin_Mode/src/main/java/org/hawkular/plugins/Mode.java
@@ -3,28 +3,48 @@
  */
  package org.hawkular.plugins;
  
-import java.util.Arrays;
+import java.util.*;
 import org.hawkular.*;
 public class Mode implements StatisticalAlgo {
 
-	int A[];
+	List<Double> elements;
+	int window_size;
 	
 	public String getPluginName() {
-		return "Mode";
+		return "Maximum";
 	}
 
-	public void setParameter (int param[]) {
-		A = param;
+	public void set_params(int size) {
+		elements = new ArrayList<Double>();
+		window_size = size;
+	}
+
+	public void pushPoint (double value) {
+		if(elements.size()<window_size)
+			elements.add(value);
+		else
+		{
+			for(int i=0;i<window_size-1;i++)
+			{
+				elements.set(i,elements.get(i+1));
+			}
+			elements.set(window_size-1, value);
+		}
 	}
 
 	public double getResult() {
-		Arrays.sort(A);
-		int size = A.length;
+		
+		int size = elements.size();
+		if(size<window_size)
+			return -1.0;
+			
+		Collections.sort(elements);
 		double res;
 		if(size%2!=0)
-			res = A[size/2];
+			res = (elements.get(size/2));
 		else
-			res = (A[size/2-1] + A[size/2])/2.0;
+			res = (elements.get(size/2-1) + elements.get(size/2))/2.0;
+		
 		return res;
 	}
 
